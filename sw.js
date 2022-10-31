57378d5df7aefbe6255cc54579225791c4641d10
jxqt <115939082+jxqt@users.noreply.github.com>
2022-10-31 22:02:58 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,19 +19,21 @@ const PRECACHE_LIST = [
   "./js/bootstrap.min.js",
   "./js/hux-blog.min.js",
   "./js/snackbar.js",
+  "./img/icon_wechat.png",
+  "./img/avatar-hux.jpg",
+  "./img/home-bg.jpg",
+  "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
   "./css/bootstrap.min.css"
-  "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
-  "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
-  "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
+  // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
+  // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
+  // "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "cdnjs.cloudflare.com"
-  "yanshuo.io",
   "huangxuan.me",
- 
- 
+  "yanshuo.io",
+  "cdnjs.cloudflare.com"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
@@ -259,10 +261,8 @@ function revalidateContent(cachedResp, fetchedResp) {
         sendMessageToClientsAsync({
           'command': 'UPDATE_FOUND',
           'url': fetched.url
-
         })
       }
     })
     .catch(err => console.log(err))
 }
-
